5483fd18fb8b532bb139a80f9991c14a8a6869cc
ke-kimura <kimura7298@yahoo.co.jp>
2023-01-08 13:23:33 +0900

    scss-commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { client } from "../libs/client";
 import Link from 'next/link'
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 
 //SSG
 export const getStaticProps = async() => {

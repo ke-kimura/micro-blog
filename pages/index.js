b63e2501a94703b73af5c19842db8f6f24d1a74f
ke-kimura <kimura7298@yahoo.co.jp>
2023-07-27 22:17:53 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { client } from "../libs/client";
 import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.scss';
 
 //SSG
@@ -15,6 +17,16 @@ export const getStaticProps = async() => {
 export default function Home( {blog} ) {
   return (
   <div className={styles.container}>
+    <div className="flex flex-col">HOME</div>
+    <span class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Badge</span>
+<span class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">Badge</span>
+<span class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">Badge</span>
+<span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Badge</span>
+<span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">Badge</span>
+<span class="inline-flex items-center rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10">Badge</span>
+<span class="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">Badge</span>
+<span class="inline-flex items-center rounded-md bg-pink-50 px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">Badge</span>
+
     {blog.map((blog) => (
         <li key={blog.id}>
           <Link href={`/blog/${blog.id}`}>
